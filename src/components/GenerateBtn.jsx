2ccc8493f779841442d4f75dca80db08396da155
Mehrab Hossain <88287873+DevMehrab@users.noreply.github.com>
2025-03-02 19:37:25 +0600

    Update GenerateBtn.jsx

--- src/components/GenerateBtn.jsx
+++ src/components/GenerateBtn.jsx
@@ -4,7 +4,7 @@ export const GenerateBtn = (e) => {
     function handleClick() {
         let prompt = localStorage.getItem('prompt') || `Generate a cat name which is beautiful . 
             give me the name only. dont send me any other extra texts. give me only and only one 
-            word and that is that cats name`
+            word and that is that cats name.`
         console.log(prompt);
 
         async function generate(param) {
